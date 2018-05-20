ebb399bc1f0bbfd400d58a630404d68098011352
胡家明 <925773276@qq.com>
2018-05-20 09:19:33 +0800

    version 1.1.2

--- SchoolShare/settings.py
+++ SchoolShare/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/admin/static'
+STATIC_URL = '/resources/'
+STATIC_ROOT = '/home/admin/resources'
